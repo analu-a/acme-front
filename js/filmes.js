279a7b439e6ef3855f0fc6a44e15d0eaa0c794c7
Ana Luiza <analuiza.azevedomorais@gmail.com>
2024-02-27 17:29:25 -0300

    finlizar header

---
+++ js/filmes.js
@@ -0,0 +1,14 @@
+// export async function getFilmes (){
+//     const url = 'http://localhost:8080/v2/FilmesACME/filmes'
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     return data.filmes
+// }
+
+// export async function getFilme (id){
+//     const url = `http://localhost:8080/v2/FilmesACME/filmes/${id}`
+//     const response = await fetch(url)
+//     const data = response.json()
+//     return data.filme[1]
+
+// }
---
+++ js/main.js
@@ -0,0 +1,41 @@
+// import { getFilmes, getFilme } from "./filmes.js"
+
+// // getFilmes()
+
+// // getFilme(1)
+
+// const filmes = [
+// {
+//     nome: "fernando leonid"
+// },
+// {
+//     nome:"Ana luiza"
+// }
+
+// ]
+
+// function criarCard (filme){
+//     const card = document.createElement('div')
+//     const title = document.createElement('h2')
+//     const duracao = document.createElement('button')
+//     title.textContent = filme.nome
+//     duracao.textContent = filme.sinopse
+
+//     card.append(title, duracao)
+//     return card
+// }
+
+// async function preencherContainer() {
+//     const container = document.querySelector('body')
+//     const filmes = await getFilmes()
+// filmes.forEach(filme => {
+//     const card = criarCard(filme)
+//     container.appendChild(card)
+    
+// });
+
+
+    
+// }
+
+// preencherContainer()
