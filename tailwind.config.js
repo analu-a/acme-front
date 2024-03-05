1279810d85b7e8044af7d102e1e7cddedeaa9833
Ana Luiza <analuiza.azevedomorais@gmail.com>
2024-03-05 17:16:00 -0300

    terminar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'vermelho-fundo': '#7D2727',
+        'gradiente': '#540606',
+        'input': '#4F0000'
+      }
+    },
+  },
+  plugins: [],
+}
+
